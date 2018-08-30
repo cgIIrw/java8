1e74cae2e5f16a677258ddc52116c3422e190053
cgIIrw <chenguo_jay@163.com>
2018-08-30 20:37:19 +0800

    add ch11/Discount

--- src/example/ch11/Discount.java
+++ src/example/ch11/Discount.java
@@ -1,4 +1,21 @@
 package ch11;
 
+import static ch11.Shop.delay;
+
 public class Discount {
+
+    public enum Code {
+        NONE(0), SILVER(5), GOLD(10), PLATINUM(15), DIAMOND(20);
+
+        private final int percentage;
+
+        Code(int percentage) {
+            this.percentage = percentage;
+        }
+    }
+
+    private static double apply(double price, Code code) {
+        delay();
+        return price * (100 - code.percentage) / 100;
+    }
 }
